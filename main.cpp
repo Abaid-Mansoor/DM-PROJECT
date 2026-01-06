48948358f58e4cb95ba0932e7bfb162892aa41be
WasifShuja <shujawasif874@gmail.com>
2026-01-06 20:29:05 +0500

    the final version

--- main.cpp
+++ main.cpp
@@ -3,22 +3,26 @@
 #include <conio.h>
 #include <fstream>
 using namespace std;
+
+// Functions to print Menus
 void printHeader();
 void printMenu();
-string generateSerialId(int serial, int shelfNo,int &checkDigit);
+void printAllSerial();
+void printBarcode(string);
+// Function to generate and validate Serial Ids
+string generateSerialId(int serial, int shelfNo, int &checkDigit);
 int generateCheckDigit(int, int);
-void readingDataFromInputSerialId(string serialId, int &shelfNo, int &serialNo, int &checkDigit);
-void writingDataInFile(string serialId, string bookName, string author,string serialIntoBinary);
 bool verifyInputFormat(string serial);
 string readingDataFromField(string record, int fieldNo);
 string verifyIfSerialIdExist(string serialId);
-int readingLastSerialFromFile();
-void writingLastSerialInFile(string);
-string concatenateSerialId(string,string);
+string concatenateSerialId(string, string);
 string intoBinary(int decimal);
-string GenerateBinaryFromSerial(int shelfNo,int serial,int checkDigit);
-void printBarcode(string);
-
+// Function for File Handling
+string GenerateBinaryFromSerial(int shelfNo, int serial, int checkDigit);
+void writingDataInFile(string serialId, string bookName, string author, int shelfNo, string serialIntoBinary);
+void readingDataFromInputSerialId(string serialId, int &shelfNo, int &serialNo, int &checkDigit);
+void writingLastSerialInFile(string);
+int readingLastSerialFromFile();
 
 main()
 {
@@ -35,11 +39,11 @@ main()
         {
 
             int lastSerial = readingLastSerialFromFile();
-            if(currentSerial!=lastSerial||lastSerial==0)
-                currentSerial=lastSerial+1;
+            if (currentSerial != lastSerial || lastSerial == 0)
+                currentSerial = lastSerial + 1;
 
             string bookName, author, serialId;
-            int shelfNo,checkDigit;
+            int shelfNo, checkDigit;
             cout << "Enter book Name: ";
             getline(cin >> ws, bookName);
             cout << "Enter author Name: ";
@@ -48,18 +52,17 @@ main()
             cin >> shelfNo;
             if (!(shelfNo >= 1 && shelfNo <= 99))
             {
-                cout<<"Enter a shelf number between 1 and 99"<<endl;
+                cout << "Enter a shelf number between 1 and 99" << endl;
                 getch();
                 continue;
             }
-            serialId = generateSerialId(currentSerial, shelfNo,checkDigit);
-            string serialToBianry=GenerateBinaryFromSerial(shelfNo,currentSerial,checkDigit);
-            cout<<"------------------------------------------------"<<endl;
-            cout <<"Serial Id: "<< serialId << endl;
-            cout<<"Barcode:"<<endl;
+            serialId = generateSerialId(currentSerial, shelfNo, checkDigit);
+            string serialToBianry = GenerateBinaryFromSerial(shelfNo, currentSerial, checkDigit);
+            cout << "------------------------------------------------" << endl;
+            cout << "Serial Id: " << serialId << endl;
+            cout << "Barcode:" << endl;
             printBarcode(serialToBianry);
-            cout<<endl;
-            writingDataInFile(serialId, bookName, author,serialToBianry);
+            writingDataInFile(serialId, bookName, author, shelfNo, serialToBianry);
             writingLastSerialInFile(to_string(currentSerial));
             currentSerial++;
             cout << "Press Any Key to continue" << endl;
@@ -77,18 +80,24 @@ main()
                 record = verifyIfSerialIdExist(serialId);
                 if (record != "")
                 {
-                    if(generateCheckDigit(serialNo, shelfNo) == checkDigit){
-                    cout<<"------------------------------------------------"<<endl;    
-                    cout << "Valid Serial ID" << endl;
-                    cout << "Book Name: " << readingDataFromField(record, 2) << endl;
-                    cout << "Author: " << readingDataFromField(record, 3) << endl;
-                    printBarcode(readingDataFromField(record, 4));
-                    cout<<endl;
-                    }else{
-                        cout<<"Invalid Check Digit!!!"<<endl;
+                    if (generateCheckDigit(serialNo, shelfNo) == checkDigit)
+                    {
+                        cout << "------------------------------------------------" << endl;
+                        cout << "Valid Serial ID" << endl;
+                        cout << "Book Name: " << readingDataFromField(record, 2) << endl;
+                        cout << "Author: " << readingDataFromField(record, 3) << endl;
+                        cout << "ShelfNo: " << readingDataFromField(record, 4) << endl;
+                        cout << "Barcode: " << endl;
+                        printBarcode(readingDataFromField(record, 5));
                     }
-                }else{
-                    cout<<"Serial not found"<<endl;
+                    else
+                    {
+                        cout << "Invalid Check Digit!!!" << endl;
+                    }
+                }
+                else
+                {
+                    cout << "Serial not found" << endl;
                 }
             }
             else
@@ -97,6 +106,10 @@ main()
             }
         }
         else if (choices == "3")
+        {
+            printAllSerial();
+        }
+        else if (choices == "4")
         {
             break;
         }
@@ -113,16 +126,18 @@ void printMenu()
 {
     cout << "1. Generate a Serial ID " << endl;
     cout << "2. Validate a Serial ID " << endl;
-    cout << "3. Exit" << endl;
+    cout << "3. All Serial IDs " << endl;
+    cout << "4. Exit" << endl;
 }
 
-string generateSerialId(int serial, int shelfNo,int &checkDigit)
+string generateSerialId(int serial, int shelfNo, int &checkDigit)
 {
-    checkDigit=(generateCheckDigit(serial, shelfNo));
+    checkDigit = (generateCheckDigit(serial, shelfNo));
     string serialId = "";
     serialId += "L-";
-    if(shelfNo<10){
-        serialId+='0';
+    if (shelfNo < 10)
+    {
+        serialId += '0';
     }
     serialId += to_string(shelfNo);
     serialId += "-";
@@ -139,20 +154,19 @@ string generateSerialId(int serial, int shelfNo,int &checkDigit)
     return serialId;
 }
 
-
 int generateCheckDigit(int serial, int shelfNo)
 {
-    string numericalPartOfSerialId=concatenateSerialId(to_string(serial),to_string(shelfNo));
+    string numericalPartOfSerialId = concatenateSerialId(to_string(serial), to_string(shelfNo));
     int size = numericalPartOfSerialId.length();
     int tempSerial = stoi(numericalPartOfSerialId);
-    
+
     int sum = 0;
     int divisor;
     for (int i = 0; i < size; i++)
     {
         divisor = pow(10, (size - 1) - i);
         sum += (tempSerial / divisor) * (i + 2);
-        
+
         tempSerial %= divisor;
     }
     return sum % 10;
@@ -162,7 +176,7 @@ void readingDataFromInputSerialId(string serialId, int &shelfNo, int &serialNo, 
 {
     int hyphenCount = 0;
     string shelfNoInString = "", serialNumberInString = "";
-    string checkDigitInString="";
+    string checkDigitInString = "";
     for (int i = 0; i < serialId.length(); i++)
     {
         if (serialId[i] == '-')
@@ -185,7 +199,7 @@ void readingDataFromInputSerialId(string serialId, int &shelfNo, int &serialNo, 
             }
         }
     }
-    
+
     shelfNo = stoi(shelfNoInString);
     serialNo = stoi(serialNumberInString);
     checkDigit = stoi(checkDigitInString);
@@ -193,7 +207,8 @@ void readingDataFromInputSerialId(string serialId, int &shelfNo, int &serialNo, 
 
 bool verifyInputFormat(string serial)
 {
-    if(serial.length()==12){
+    if (serial.length() == 12)
+    {
         for (int i = 0; i < serial.length(); i++)
         {
             if (serial[0] != 'L')
@@ -210,8 +225,7 @@ bool verifyInputFormat(string serial)
             if (i == 2 || i == 3 || (i <= 5 && i >= 9) || i == 11)
             {
                 int ascci = int(serial[i]);
-            
-            
+
                 if (!(ascci >= 48 && ascci <= 57))
                 {
                     return false;
@@ -226,11 +240,11 @@ bool verifyInputFormat(string serial)
     return true;
 }
 
-void writingDataInFile(string serialId, string bookName, string author,string serialToBinary)
+void writingDataInFile(string serialId, string bookName, string author, int shelfNo, string serialToBinary)
 {
     fstream file;
     file.open("data.txt", ios::app);
-    file << serialId << "," << bookName << "," << author << ","<<serialToBinary<<",";
+    file << serialId << "," << bookName << "," << author << "," << to_string(shelfNo) << "," << serialToBinary << ",";
     file << endl;
     file.close();
 }
@@ -258,7 +272,7 @@ string verifyIfSerialIdExist(string serialId)
     fstream file;
     file.open("data.txt", ios::in);
     string record = "";
-    while (getline(file,record))
+    while (getline(file, record))
     {
         if (readingDataFromField(record, 1) == serialId)
         {
@@ -270,90 +284,136 @@ string verifyIfSerialIdExist(string serialId)
     return record;
 }
 
-int readingLastSerialFromFile(){
+int readingLastSerialFromFile()
+{
     string lastSerial;
     fstream file;
-    file.open("lastSerial.txt",ios::in);
+    file.open("lastSerial.txt", ios::in);
 
-    if(!(file>>lastSerial)){
-        lastSerial='0';
+    if (!(file >> lastSerial))
+    {
+        lastSerial = '0';
     }
     file.close();
     return stoi(lastSerial);
 }
 
-void writingLastSerialInFile(string lastSerial){
+void writingLastSerialInFile(string lastSerial)
+{
     fstream file;
-    file.open("lastSerial.txt",ios::out);
-    file<<lastSerial;
+    file.open("lastSerial.txt", ios::out);
+    file << lastSerial;
     file.close();
 }
 
-string concatenateSerialId(string serial,string shelfNo){
-    string numericalpartofserialId="";
-    if(shelfNo.length()==1){
-        numericalpartofserialId+='0';
+string concatenateSerialId(string serial, string shelfNo)
+{
+    string numericalpartofserialId = "";
+    if (shelfNo.length() == 1)
+    {
+        numericalpartofserialId += '0';
     }
-    numericalpartofserialId+=shelfNo;
-    for(int i=5-serial.length();i>0;i--){
-        numericalpartofserialId+='0';
+    numericalpartofserialId += shelfNo;
+    for (int i = 5 - serial.length(); i > 0; i--)
+    {
+        numericalpartofserialId += '0';
     }
-    numericalpartofserialId+=serial;
+    numericalpartofserialId += serial;
     return numericalpartofserialId;
 }
 
-void printHeader(){
-    cout<<"----------------------------------------------"<<endl;
-    cout<<"\tBook Serial Id Generation"<<endl;
-    cout<<"\t And Verification System"<<endl;
-    cout<<"----------------------------------------------"<<endl;
+void printHeader()
+{
+    cout << "----------------------------------------------" << endl;
+    cout << "\tBook Serial Id Generation" << endl;
+    cout << "\t And Verification System" << endl;
+    cout << "----------------------------------------------" << endl;
 }
 
-string GenerateBinaryFromSerial(int shelfNo,int serial,int checkDigit){
-    string temp="",binaryTemp="";
+string GenerateBinaryFromSerial(int shelfNo, int serial, int checkDigit)
+{
+    string temp = "", binaryTemp = "";
 
-    if(shelfNo<10)
-        temp+='0';
-    temp+=(to_string(shelfNo)+to_string(serial)+to_string(checkDigit));
-    for(int i=0;i<temp.length();i++){
-        if(temp[i]=='0'){
-            binaryTemp+="0000";
-        }else{
-            binaryTemp+=intoBinary(temp[i]-'0');
+    if (shelfNo < 10)
+        temp += '0';
+    temp += (to_string(shelfNo) + to_string(serial) + to_string(checkDigit));
+    for (int i = 0; i < temp.length(); i++)
+    {
+        if (temp[i] == '0')
+        {
+            binaryTemp += "0000";
         }
-
-
-    }    
+        else
+        {
+            binaryTemp += intoBinary(temp[i] - '0');
+        }
+    }
     return binaryTemp;
 }
 
-string intoBinary(int decimal){
-    string binary="",fourBitBinary="";
-    while(true){
-        binary+=to_string(decimal%2);
-        if(decimal<2){
-            binary+=decimal;
+string intoBinary(int decimal)
+{
+    string binary = "", fourBitBinary = "";
+    while (true)
+    {
+        binary += to_string(decimal % 2);
+        if (decimal < 2)
+        {
+            binary += decimal;
 
             break;
-        }else{
-        decimal/=2;
+        }
+        else
+        {
+            decimal /= 2;
         }
     }
-    for(int i=4-binary.length();i>=0;i--){
-        fourBitBinary+='0';
+    for (int i = 4 - binary.length(); i >= 0; i--)
+    {
+        fourBitBinary += '0';
     }
-    fourBitBinary+=binary;
+    fourBitBinary += binary;
     return fourBitBinary;
 }
 
-void printBarcode(string binary){
-    cout<<"|||";
-    for(int i=0;i<binary.length();i++){
-        if(binary[i]=='0'){
-            cout<<" ";
-        }else if(binary[i]=='1'){
-            cout<<"|";
+void printBarcode(string binary)
+{
+    cout << "|||";
+    for (int i = 0; i < binary.length(); i++)
+    {
+        if (binary[i] == '0')
+        {
+            cout << " ";
+        }
+        else if (binary[i] == '1')
+        {
+            cout << "|";
         }
     }
-    cout<<"|||";
+    cout << "|||";
+    cout << endl;
+}
+
+void printAllSerial()
+{
+    bool recordExist = false;
+    string record;
+    fstream file;
+    file.open("data.txt", ios::in);
+    while (getline(file, record))
+    {
+        recordExist = true;
+        cout << "------------------------------------------" << endl;
+        cout << "Book:      " << readingDataFromField(record, 1) << endl;
+        cout << "Author:    " << readingDataFromField(record, 2) << endl;
+        cout << "Serial ID: " << readingDataFromField(record, 3) << endl;
+        cout << "Shelf No: " << readingDataFromField(record, 4) << endl;
+        cout << "Barcode: " << endl;
+        printBarcode(readingDataFromField(record, 5));
+        cout << "------------------------------------------" << endl;
+    }
+    if (!recordExist)
+    {
+        cout << "No Serial IDs" << endl;
+    }
 }
